c25b4097cf92f2c4aac5636db1344956b87b8dce
TonyAlvin <2680641586@qq.com>
2019-09-28 14:53:10 +0800

    add file read and write

--- source/core.c
+++ source/core.c
@@ -11,8 +11,28 @@ extern int MouseX;
 extern int MouseY;
 extern int MouseB;
 
+void CountRunTimes(void)
+{
+    int times = 0;
+    FILE *fp;
+    if ((fp = fopen(".\\resource\\data.dat", "rb+")) == NULL)
+    {
+        printf("open data.dat fail~");
+        delay(3000);
+        exit(1);
+    }
+    fscanf(fp, "###%d###", &times);
+    printf("No. %d Run", times);
+    times++;
+    rewind(fp);
+    fprintf(fp, "###%d###", times);
+    fclose(fp);
+    delay(1000);
+    return;
+}
 int main()
 {
+    CountRunTimes();
     SVGA_Init();
     //Set_Pal_File(".\\resource\\svga\\win.act");
     setbkcolor(BLACK);
@@ -23,7 +43,7 @@ int main()
     PutHZ24(500, 700, "³ÂÕ×ÀÊ111¹þ¹þ¹þ", WHITE, 1, 2, 0, 0);
     PutHZ16(500, 730, "³ÂÕ×ÀÊ111¹þ¹þ¹þ", WHITE, 1, 1, 0, 0);
     PutHZ12(500, 750, "³ÂÕ×ÀÊ111¹þ¹þ¹þ", WHITE, 1, 1, 0, 0);
-    DrawTrafficLight(100,200,2,RED);
+    DrawTrafficLight(100, 200, 2, RED);
     while (1)
     {
         MouseRead();
